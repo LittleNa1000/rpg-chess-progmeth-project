0372f6f8c6d88741db8119ea4ca964dab44d4825
TumKtp <tumktp.dev@gmail.com>
2022-05-15 16:31:38 +0700

    Add all units

--- src/base/BaseUnit.java
+++ src/base/BaseUnit.java
@@ -5,12 +5,16 @@ public abstract class BaseUnit implements Moveable {
     private String name;
     private int power;
     private int currentHealth;
+    private int stunRoundLeft;
+    private int venomRoundLeft;
 
     public BaseUnit(String name, int maxHealth, int power) {
         this.maxHealth = maxHealth;
         this.name = name;
         this.power = power;
         this.currentHealth = maxHealth;
+        this.stunRoundLeft = 0;
+        this.venomRoundLeft = 0;
     }
 
     @Override
---
+++ src/unit/FlyingUnit.java
@@ -0,0 +1,23 @@
+package unit;
+
+import base.Attackable;
+import base.BaseUnit;
+import constant.UnitConstant;
+
+public class FlyingUnit extends BaseUnit implements Attackable {
+    public FlyingUnit() {
+        super(UnitConstant.FLYING_UNIT_NAME, UnitConstant.FLYING_UNIT_MAX_HP, UnitConstant.FLYING_UNIT_POWER);
+    }
+
+    @Override
+    public void move() {
+        // TODO Auto-generated method stub
+        super.move();
+    }
+
+    @Override
+    public void attackUnit() {
+        // TODO Auto-generated method stub
+
+    }
+}
