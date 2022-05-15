264873799b50b6e6c6c2dda41fa8359b8fb936c1
TumKtp <tumktp.dev@gmail.com>
2022-05-15 21:21:46 +0700

    Add BoardConstant

--- src/application/Game/Board.java
+++ src/application/Game/Board.java
@@ -1,5 +1,6 @@
 package application.Game;
 
+import constant.BoardConstant;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -9,21 +10,18 @@ import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 
 public class Board extends GridPane {
-  private final int numCols = 8;
-  private final int numRows = 8;
-
   public Board() {
     super();
     setPadding(new Insets(15, 15, 15, 15));
     setGridLinesVisible(true);
-    for (int i = 0; i < numCols; i++) {
+    for (int i = 0; i < BoardConstant.COLOUMN_NUMBER; i++) {
       ColumnConstraints colConst = new ColumnConstraints();
-      colConst.setPercentWidth(100.0 / numCols);
+      colConst.setPercentWidth(100.0 / BoardConstant.COLOUMN_NUMBER);
       getColumnConstraints().add(colConst);
     }
-    for (int i = 0; i < numRows; i++) {
+    for (int i = 0; i < BoardConstant.ROW_NUMBER; i++) {
       RowConstraints rowConst = new RowConstraints();
-      rowConst.setPercentHeight(100.0 / numRows);
+      rowConst.setPercentHeight(100.0 / BoardConstant.ROW_NUMBER);
       getRowConstraints().add(rowConst);
     }
     setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
---
+++ src/constant/BoardConstant.java
@@ -0,0 +1,6 @@
+package constant;
+
+public class BoardConstant {
+    static final public int COLOUMN_NUMBER = 8;
+    static final public int ROW_NUMBER = 8;
+}
